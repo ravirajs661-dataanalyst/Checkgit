f484371a0e13018435c19ea1ba5013c2c5e266b8
raviraj <ravirajs661@gmail.com>
2026-08-07 12:16:39 +0530

    If condidion

---
+++ If Condition.py
@@ -0,0 +1,7 @@
+f_num=int(input("enter first number"))
+s_num=int(input("enter second number"))
+add=f_num+s_num
+if add>30:
+    print("you are elegibal to voting")
+else:
+    print("ypu are not elegibal to voting")
