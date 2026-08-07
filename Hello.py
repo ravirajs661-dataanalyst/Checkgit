7aea84d0f9838f24f04b39d37d2df048eafada3b
raviraj <ravirajs661@gmail.com>
2026-08-07 11:46:13 +0530

    if condition

--- Hello.py
+++ Hello.py
@@ -3,17 +3,17 @@
 # res = a+b
 # print(res)
 
-# num= int(input("enter the number :"))
-# if num>20:
-#     print("Hello World")
+num= int(input("enter the number :"))
+if num>20:
+    print("Hello World")
 
-""" age=int(input("enter the age:"))
+age=int(input("enter the age:"))
 if age>18:
-    print("you are eligible to voting") """
+    print("you are eligible to voting")
 
-# age=int(input("enter your age"))
-# if age>18:
-#     print("you are eligible to voting")
-# else:
-#     print("you are not eligible to votiong")
+age=int(input("enter your age"))
+if age>18:
+    print("you are eligible to voting")
+else:
+    print("you are not eligible to votiong")
 
