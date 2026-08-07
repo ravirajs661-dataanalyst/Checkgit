dd667606a8e49e03c1ccb27158bb60a4bf04103c
raviraj <ravirajs661@gmail.com>
2026-08-07 17:31:59 +0530

    case function

--- If Condition.py
+++ If Condition.py
@@ -39,9 +39,9 @@
 
 #nestd if condition 
 
-age= int(input("enter your age"))
-citizen_score=int(input("inter your citizen score"))
-national_score=int(input("enter your national score:"))
+# age= int(input("enter your age"))
+# citizen_score=int(input("inter your citizen score"))
+# national_score=int(input("enter your national score:"))
 # if age>18:
 #     if citizen_score>40:
 #         print("you are eligibal to vote")
@@ -50,19 +50,91 @@ national_score=int(input("enter your national score:"))
 # else:
 #     print("you are just a kid")
 
-if age>18:
-    if citizen_score>40:
-        if national_score>60:
-            print("you are eligible to vote")
-        else:
-            print("you are not eligible to vote. increase your national score")
-    else:
-        print("you are not eligible to vote. increase your citizen score")
-else:
-    if age>=16:
-        print("wiat for 1-2 years")
-    else:
-        print("you are just a kid bro")
+# if age>18:
+#     if citizen_score>40:
+#         if national_score>60:
+#             print("you are eligible to vote")
+#         else:
+#             print("you are not eligible to vote. increase your national score")
+#     else:
+#         print("you are not eligible to vote. increase your citizen score")
+# else:
+#     if age>=16:
+#         print("wiat for 1-2 years")
+#     else:
+#         print("you are just a kid bro")
+
+
+# mark=int(input("enter your mark"))
+# if mark>90:
+#     print("A")
+# elif mark>80:
+#     print("B")
+# elif mark>60:
+#     print("C")
+# elif mark>40:
+#     print("Pramoted")
+# else:
+#     print("fail")
+
+# mark=int(input("enter your mark"))
+# if mark>=40:
+#     print("pass")
+# else:
+#     print("Fail")
+
+# same code ko ham kuchh is tarah se likh sakte hai
+# print("pass" if mark>=40 else "Fail")
+
+
+#match case functaiion kaise use karte hai 
+# value= int(input("enter a number :"))
+
+# match value:
+#     case 1:
+#         print("one")
+#     case 2:
+#         print("two")
+#     case 3:
+#         print("three")
+#     case _:
+#         print("invalid number")
+
+# day= int(input("enter a day(1-7) : "))
+# match day:
+#     case 1:
+#         print("monday")
+#     case 2:
+#         print("tuesday")
+#     case 3:
+#         print("wednesday")
+#     case 4:
+#         print("thursday")
+#     case 5:
+#         print("friday")
+#     case 6:
+#         print("saturday")
+#     case 7:
+#         print("sunday")
+#     case _:
+#         print("invalid day")
+
+
+# real life example in calculator method
+num1= int(input("enter first num :"))
+num2= int(input("input secind num :"))
+operator= input("enter opreator (+,-,*,/) :")
+
+match operator :
+    case "+":
+        print(num1+num2)
+    case "-":
+        print(num1-num2)
+    case "*":
+        print(num1*num2)
+    case "/":
+        print(num1/num2)
+
 
 
 
