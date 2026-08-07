506273de2629e7e575757c9e8373865b310c1813
raviraj <ravirajs661@gmail.com>
2026-08-07 12:01:32 +0530

    updated Hello.py

--- Hello.py
+++ Hello.py
@@ -3,17 +3,25 @@
 # res = a+b
 # print(res)
 
-num= int(input("enter the number :"))
-if num>20:
-    print("Hello World")
+# num= int(input("enter the number :"))
+# if num>20:
+#     print("Hello World")
 
-age=int(input("enter the age:"))
-if age>18:
-    print("you are eligible to voting")
+# age=int(input("enter the age:"))
+# if age>18:
+#     print("you are eligible to voting")
 
-age=int(input("enter your age"))
-if age>18:
-    print("you are eligible to voting")
+# age=int(input("enter your age"))
+# if age>18:
+#     print("you are eligible to voting")
+# else:
+#     print("you are not eligible to votiong")
+
+f_num=int(input("enter first number"))
+s_num=int(input("enter second number"))
+add=f_num+s_num
+if add>30:
+    print("you are elegibal to voting")
 else:
-    print("you are not eligible to votiong")
+    print("ypu are not elegibal to voting")
 
