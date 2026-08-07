5703789dab07f45575ef280de729c9456b9f1714
raviraj <ravirajs661@gmail.com>
2026-08-07 13:57:08 +0530

    update if condition

--- If Condition.py
+++ If Condition.py
@@ -9,14 +9,32 @@
 # age>=16 and age<18 - wait for 1-2 year more.
 # age<16 - you are just a kid
  
-age=int(input("enter you age"))
-if age>=18 and age<110:
-    print("Hey man! ofcourse you can vote")
-elif age>=18 and age<21:
-    print("Hello new Voter")
-elif age<16 and age<18:
-    print("you are just a kid")
+# age=int(input("enter you age"))
+# if age>=18 and age<110:
+#     print("Hey man! ofcourse you can vote")
+# elif age>=18 and age<21:
+#     print("Hello new Voter")
+# elif age<16 and age<18:
+#     print("you are just a kid")
+# else:
+#     print("Go to home")
+
+# wap to check number is positive, negative or Zero
+
+num=float(input("enter the number"))
+if num>0:
+    print("positive number")
+elif num<0:
+    print("negative number")
 else:
-    print("Go to home")
+    print("Zero")
+
+# wap to check a number and display the even or odd
+
+num=int(input("enter the number :"))
+if num % 2==0:
+    print("Even Number")
+else:
+    print("Odd number")
 
 
