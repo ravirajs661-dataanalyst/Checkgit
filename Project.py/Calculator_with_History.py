9a982f33e7589fb4c0eac89b0aa4f78b8c5f62b2
raviraj <ravirajs661@gmail.com>
2026-08-26 18:17:52 +0530

    Project

--- Project.py/Calculator_with_History.py
+++ Project.py/Calculator_with_History.py
@@ -21,9 +21,37 @@ def save_to_history(equcation, result):
     file.write(equcation + "=" + str(result) + "\n")
     file.close()
 
+def calcultor(user_input):
+    parts=user_input.split()
+    if len(parts) != 0:
+        print("Invalid input! Use Formate. Number Operator Number. e.g.(8+8)")
+        return
 
+    num1=float(parts[0])
+    op=parts[1]
+    num2=float(parts[2])
 
+    if op == "+":
+        result= num1 + num2
+    elif op == "-":
+        result= num1 - num2
+    elif op == "*":
+        result= num1 * num2
+    elif op == "/":
+        if num2 == 0:
+            print("You can not devide by zero!")
+            return
+        result= num1/num2
+    else:
+        print("Invalid Operator! Use only(=,-,*,/)")
+        return
 
+    if int(result) == result:
+        result = int(result)
+    print("Result:", result)
+    save_to_history(user_input, result)
 
+def main():
+    print("___SIMPLE CALCULATOR (Type History, clear, exit)")
 
     
