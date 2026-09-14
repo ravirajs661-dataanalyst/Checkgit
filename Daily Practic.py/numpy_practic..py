c1a4a0bb46169630a454dc135fe17d4f6a71ffa8
raviraj <ravirajs661@gmail.com>
2026-09-14 17:35:07 +0530

    Numpy

---
+++ Daily Practic.py/numpy_practic..py
@@ -0,0 +1,42 @@
+import numpy as np
+
+# marks=np.array([65,90,80,45,70,55])
+
+#properties
+"""
+print(marks.ndim)
+print(marks.shape)
+print(marks.size)
+print(marks.dtype)
+"""
+
+# Numpy Operations----
+"""
+print(np.sum(marks))
+print(np.mean(marks))
+print(np.max(marks))
+print(np.min(marks))
+"""
+# print(marks[marks>50])
+
+# Array Properties find this: ndim, shape, size, dtype
+"""
+marks=[10,20,30,40,50,60]
+
+mark= (np.array(marks))
+print(mark.ndim)
+print(mark.shape)
+print(mark.size)
+print(mark.dtype)
+
+"""
+
+# Mathematical Operation Find this: Total, average, maximum, minimun
+marks=np.array([10,20,30,40,50,60])
+"""
+print(np.sum(marks))
+print(np.mean(marks))
+print(np.max(marks))
+print(np.min(marks))
+"""
+
