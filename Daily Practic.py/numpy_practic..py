70c42a839a7400e84d526bfec207d8a3f6c9af7d
raviraj <ravirajs661@gmail.com>
2026-09-15 14:32:26 +0530

    numpy

--- Daily Practic.py/numpy_practic..py
+++ Daily Practic.py/numpy_practic..py
@@ -32,11 +32,43 @@ print(mark.dtype)
 """
 
 # Mathematical Operation Find this: Total, average, maximum, minimun
-marks=np.array([10,20,30,40,50,60])
+# marks=np.array([10,20,30,40,50,60])
 """
 print(np.sum(marks))
 print(np.mean(marks))
 print(np.max(marks))
 print(np.min(marks))
+"""
+"""
+arr=np.array([10,20,30,40,50,60])
+print(arr[3])
+print(arr[-1])
+
+print(arr[1:4]) #---20,30,40 print
+print(arr[:4])  #first 4 elements
+print(arr[3:])  #last 3 elements
+"""
+
+"""
+arr=np.array([10,20,30,40,50,60,70,80])
+print(arr[::2])  # har 2nd elements 
+print(arr[::-1]) # reverse elements print
+"""
+
+"""
+#2D Indexing
+arr=np.array([[10,20,30],
+              [40,50,60],
+              [70,80,90]
+              ])
+
+print(arr[2,1])  # 80 print hoga
+print(arr[0,2])  # 30 print hoga
+
+# second row print karo
+print(arr[1])  
+
+#3rd column print karo
+print(arr[:,2])
 """
 
