0773cf90d213b8a2229fe314bbe2b79937d2a1e6
raviraj <ravirajs661@gmail.com>
2026-09-17 18:06:17 +0530

    Practic

--- Daily Practic.py/practic question.py
+++ Daily Practic.py/practic question.py
@@ -144,8 +144,8 @@ total = 0
 #     print(i**3)
 
 # Print each character of a given string.
-name = "raviraj"
-i=0
+# name = "raviraj"
+# i=0
 # while i <len(name):
 #     print(name[i])
 #     i=i+1
@@ -162,10 +162,217 @@ dup=[]
 #         dup.append(i)
 # print(dup)
 
+"""
 students=("Ravi",26,"Bca",8.5)
 name, age, course, gpa=students
 print(name, age, course, gpa)
 
 numbers= [1,2,3,4,5,6,7]
 print(numbers)
+
+"""
+
+#------------- Iterator Example ------------ 
+"""
+list=[2,4,6,8,10,12]
+it=iter(list)
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
+
+"""
+"""
+name="Ravi_raj"
+it=iter(name)
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
+
+"""
+"""
+#  Practic File On studies file------
+num1=int(input("Enter First Number: "))
+num2=int(input("Enter Second Number: "))
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+print(num1/num2)
+"""
+"""
+# Take a number and check whether it is positive, negative, or zero.
+num=int(input("Enter a Number: "))
+if num >0:
+    print("positive number")
+elif num<0:
+    print("negative number")
+else:
+    print("Zero")
+"""
+"""
+# Take a person's age and print:
+age=int(input("Enter age: "))
+if age < 13:
+    print("child")
+elif age >13 and age<19:
+    print("Tenager")
+elif age >20 and age < 59:
+    print("Adult")
+else:
+    print("Senior")
+"""
+
+"""
+# Take three numbers and find the largest number without using max().
+num=(3,5,7,9,14,52,2)
+largest=0
+for i in num:
+    if i > largest:
+        largest=i
+print(largest)
+"""
+"""
+# Create a simple calculator using match-case:
+num1=int(input("Enrer first number: "))
+num2=int(input("Enter second number: "))
+operator=input("Enter a operator: (+,-,*,/)")
+
+while True:
+    if operator == "+":
+        print(num1 + num2)
+        break
+    elif operator == "-":
+        print(num1 - num2)
+        break
+    elif operator == "*":
+        print(num1 * num2)
+        break
+    elif operator == "/":
+        print(num1 /num2)
+        break
+    else:
+        print("Invalid Operator")
+    break
+
+"""
+"""
+# Part 2: Strings, Indexing & Slicing
+text = "Python Programming"
+
+print(text[0]) # print first index
+print(text[:-2:-1]) # print last index
+print(text[:7])  # print first 6 index
+print(text[-11:]) # print last 11 cahrcter
+print(text[::-1]) # Reverse Indexing
+"""
+"""
+# Count the number of vowels in:
+text = "Python Programming"
+count=0
+for i in text:
+    if i in "aeiouAEIOU":
+        count=count + 1
+print(count)
+"""
+"""
+# Take a string from the user and check whether it is a palindrome.
+text = input("Enter a string: ")
+reverse=text[::-1]
+
+if text == reverse:
+    print("Palindrone")
+else:
+    print("Not plaindrone")
+"""
+"""
+# Create a new string where every space is replaced with _.
+text = "data analysis"
+print(text.replace(" ","_"))
+"""
+"""
+# Take a full name from the user and print:
+name=input("enter your name: ")
+n=name.split()
+print("First Name: ", n[0])
+print("Last Name: ", n[-1])
+"""
+"""
+# Find the sum of all elements using a loop, without sum().
+numbers = [10, 20, 30, 40, 50]
+total= 0
+for i in numbers:
+    total=total+i
+print(total)
+"""
+
+"""
+# Find the largest and smallest number in a list without using max() and min().
+numbers = [10, 20, 30, 40, 50]
+largest = numbers[0]
+smallest = numbers[0]
+
+for i in numbers:
+    if i > largest:
+        largest = i
+
+    if i < smallest:
+        smallest = i
+
+print("Largest Number: ", largest)
+print("Smallest Number: ", smallest)
+"""
+"""
+# Remove duplicate values.
+numbers = [10, 20, 10, 30, 20, 40, 10]
+new_list=[]
+for i in numbers:
+    if i not in new_list:
+        new_list.append(i)
+
+print(new_list)
+"""
+"""
+# Create two sets:
+A = {10, 20, 30, 40}
+B = {30, 40, 50, 60}
+
+print(A|B)
+print(A&B)
+print(A-B)
+print(A^B)
+"""
+"""
+# Given a tuple: Print it in reverse order.
+data = (10, 20, 30, 40, 50)
+print(data[::-1])
+"""
+"""
+# Create a dictionary containing: Print each key and value using a loop.
+students={
+    "name" : "ravi raj",
+    "age" :22,
+    "city" : "patna",
+    "course" : "Python, sql, excel"
+}
+
+for key, value in students.items():
+    print(key,":",value)
+"""
+
+# Find the subject with the highest marks without using max().
+marks = {
+    "Math": 85,
+    "English": 72,
+    "Python": 91,
+    "SQL": 88
+}
+
+
 
